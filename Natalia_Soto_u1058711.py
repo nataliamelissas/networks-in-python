6291cc37f8b82f7391de23101cafc1e5f326feb3
Natalia Soto <nataliamelissas@gmail.com>
2020-01-25 19:36:36 -0700

    Proxy accepts multiple client connections

--- Natalia_Soto_u1058711.py
+++ Natalia_Soto_u1058711.py
@@ -1,54 +1,38 @@
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+import threading
+import argparse
 import re
+
 
 # Important socket connection data
 proxySocket = None
-clientSocket = None
-serverSocket = None
-
-clientRequest = ""
-hostUrl = ""
-hostUrlParam = "/"
-hostPort = 80
-clientHeaders = []
 
 
 def createProxyListenerSocket():
-    global proxySocket
     """Creates an initial listener TCP socket for the proxy"""
-    # Get port # and server address
-    serverPort = ""
-    while 1:
-        try:
-            serverPort = input("Specify a port #: ")
-            break
-        except Exception:
-            continue
-    serverAddress = 'localhost'
+    global proxySocket
+    parser = argparse.ArgumentParser(
+        description="""This is a multi-client web proxy server.
+        Default port number is 8000 unless supplied in commandline.""")
+    parser.add_argument('--port', metavar='port', type=str,
+                        nargs='?', default='8000')
+    args = parser.parse_args()
+    proxyAddress = 'localhost'
     proxySocket = socket(AF_INET, SOCK_STREAM)
-    print 'Got a socket with fd:', proxySocket.fileno()
-    # allow us to quickly reuse the port number...
     proxySocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     try:
-        proxySocket.bind((serverAddress, serverPort))
-    except Exception:
-        print "Invalid port number. Restart the program and try again."
-        exit()
-    print 'Bound to:', proxySocket.getsockname()
-    proxySocket.listen(1)
-
-
-def listenForClient():
-    """Listens for a single client connection"""
-    print 'Listening for requests'
-    global clientSocket
-    clientSocket, addr = proxySocket.accept()
-    print 'Accepted connection from:', clientSocket.getpeername(
-    ), ' fd: ', clientSocket.fileno()
+        proxySocket.bind((proxyAddress, int(args.port)))
+        print 'Running the proxy on:', proxySocket.getsockname()
+        proxySocket.listen(1)
+    except Exception as e:
+        msg = "Restart the program. Failed to bind proxy on host: %s to port: %s because: %s" % (
+            proxyAddress, args.port, e)
+        raise SystemExit(msg)
+    print 'Listening for clients...'
 
 
 def sendResponse(responseCode, socket):
-    print "Sending response code: ", responseCode
+    # print "Sending response code: ", responseCode
     if (responseCode == 400):
         socket.send("HTTP/1.0 400 Bad Request\r\n")
         socket.close()
@@ -57,44 +41,44 @@ def sendResponse(responseCode, socket):
         socket.close()
     if (responseCode == 200):
         socket.send("HTTP/1.0 200 OK\r\n")
-
     return
 
 
-def processClientData():
-    """Receives incoming data 1024 bytes at a
-    time from the client and processes it.
+def processClientData(clientSocket):
+    """Receives incoming byte data from the client and processes it.
     Specifically expects a valid HTTP request"""
-    global clientRequest
     clientRequest = ""
-    while 1:
+    clientHeaders = []
+    hostUrl = ""
+    hostPort = 80
+    hostUrlParam = "/"
+
+    while True:
         incoming = clientSocket.recv(1024)
         if incoming == "\r\n":
             break
         clientRequest += incoming
 
-    print "Provided HTTP Request:\n ", clientRequest
+    # print "Provided HTTP Request:\n ", clientRequest
 
     # Split request by new lines and check request line
     splitClientRequest = clientRequest.splitlines()
     requestPattern = re.compile(
-        "^[A-Za-z]+ (?i)(https?|ftp)://[^\s/$.?#].[^\s]* HTTP/1.0$")
+        "^[A-Za-z]+ (?i)(http|ftp)://[^\s/$.?#].[^\s]* HTTP/1.0$")
     isValidRequestLine = requestPattern.match(splitClientRequest[0])
     if (not isValidRequestLine):
         # Send bad request to connection socket
         sendResponse(400, clientSocket)
         return False
-    print("Valid request line provided.")
+    # print("Valid request line provided.")
     providedHttpMethod = splitClientRequest[0].partition(' ')[0]
     if (not "GET" == providedHttpMethod):
         sendResponse(501, clientSocket)
         return False
-    print "The provided http method: ", providedHttpMethod
+    # print "The provided http method: ", providedHttpMethod
     hostAbsUrl = splitClientRequest[0].split(' ')[1]
-    print "The provided absolute host url: ", hostAbsUrl
-    global hostUrl
-    global hostPort
-    global hostUrlParam
+    # print "The provided absolute host url: ", hostAbsUrl
+
     # Extract hostUrl, port, and path from the absolute url
     hostUrl = hostAbsUrl[7:]  # 7 = length of "http://"
     colonPos = hostUrl.find(":")
@@ -126,68 +110,79 @@ def processClientData():
             sendResponse(400, clientSocket)
             return False
         clientHeaders.append(splitClientRequest[i])
-        print("Valid header provided.")
+        # print("Valid header provided.")
 
     # Client request success
-    print "Sending success reponse back to client"
+    # print "Sending successful reponse back to client"
     sendResponse(200, clientSocket)
-    return True
+    return clientHeaders, hostUrl, hostPort, hostUrlParam
 
 
-def connectToHost():
+def connectToRemoteServer(clientSocket, hostUrl, hostPort):
     """Connects to remote host server with data
     provided by the client"""
     print "Connecting to remote host..."
-    global serverSocket
-    hostAddress = hostUrl
+    # global serverSocket
     serverSocket = socket(AF_INET, SOCK_STREAM)
-    print 'Bound to: (after socket call)', serverSocket.getsockname()
-    serverSocket.connect((hostAddress, hostPort))
-    print 'Bound to: (after connect call)', clientSocket.getsockname()
+    print '\tBound to: (after socket call)', serverSocket.getsockname()
+    serverSocket.connect((hostUrl, hostPort))
+    print '\tBound to: (after connect call)', clientSocket.getsockname()
+    return serverSocket
 
 
-def forwardRequestToHost():
+def forwardRequestToRemoteServer(url, param, headers, serverSocket):
     """Forwards client request to the remote host"""
-    # Form http request line
-    hostRequest = "GET " + hostUrlParam + " HTTP/1.0\nHost: " + \
-        hostUrl + "\nConnection: close"
+    hostRequest = "GET " + param + " HTTP/1.0\nHost: " + \
+        url + "\nConnection: close"
 
     # Attach any headers
-    for x in range(len(clientHeaders)):
+    for x in range(len(headers)):
         hostRequest += "\n"
-        if (not clientHeaders[x].lower() == "connection: keep-alive"):
-            hostRequest += clientHeaders[x]
+        if (not headers[x].lower() == "connection: keep-alive"):
+            hostRequest += headers[x]
     hostRequest += "\r\n\r\n"
-
-    print "Request to host:\n", hostRequest
+    # print "Sending client request to remote server..."
+    print "Sending client request to remote server...\n", hostRequest
     serverSocket.send(hostRequest)
     hostResponse = ""
     temp = serverSocket.recv(1024)
     while (temp):
         hostResponse += temp
         temp = serverSocket.recv(1024)
+    return hostResponse
 
-    print 'From host:', hostResponse
-    print 'Sending host response to client via proxy...'
-    clientSocket.send(hostResponse)
+
+def sendResponseToClient(clientSocket, hostResponse):
+    """Send response from the remote server back to client through the proxy"""
+    print 'Sending remote server response back to client...'
+    clientSocket.sendall(hostResponse)
+
+
+def onNewClient(clientSocket, connectionAddr):
+    ip = connectionAddr[0]
+    port = connectionAddr[1]
+    print 'New client connection from IP: %s, and port: %s' % (ip, port)
+    headers, hostUrl, hostPort, hostUrlParam = processClientData(
+        clientSocket)
+    serverSocket = connectToRemoteServer(clientSocket, hostUrl, hostPort)
+    hostResponse = forwardRequestToRemoteServer(
+        hostUrl, hostUrlParam, headers, serverSocket)
+    sendResponseToClient(clientSocket, hostResponse)
+    print 'Success. Closing connection to client at IP: %s, and port: %s' % (
+        ip, port)
+    clientSocket.close()
 
 
 # 1 - Wait for connection
+print "Use '--port [port]' command argument on initial run to set a custom proxy socket port."
 createProxyListenerSocket()
 
-while 1:
-    # 2 - Wait for client connection
-    listenForClient()
-
-    # 3 - Process data from client
-    connected = processClientData()
-
-    # 4 - Connect with remote host
-    if (connected):
-        connectToHost()
-
-        # 5 - Request remote host webpage
-        forwardRequestToHost()
-
-    # Close connection with client
-    clientSocket.close()
+# 2 - Accept multiple client connections
+while True:
+    try:
+        client, ip = proxySocket.accept()
+        threading._start_new_thread(onNewClient, (client, ip))
+    except KeyboardInterrupt as e:
+        raise SystemExit('Gracefully shutting down the server.')
+    except Exception as e:
+        print 'Program threw an exception: ', e
